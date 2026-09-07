aeabc660091d439ad52d4698d5d52aade5cb08d1
Nicko <cjnickorosales@gmail.com>
2026-09-07 12:47:27 +0800

    feat: add transaction tracking views and navigation routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
-import { Routes, Route } from 'react-router';
-import { MainLayout } from './layouts/MainLayout';
-import Dashboard from './pages/Dashboard';
-import Records from './pages/Records';
-import AddTransaction from './pages/AddTransaction';
-import TransactionDetail from './pages/TransactionDetail';
-import CalendarView from './pages/CalendarView';
-import Summary from './pages/Summary';
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="records" element={<Records />} />
-        <Route path="add" element={<AddTransaction />} />
-        <Route path="transaction/:id" element={<TransactionDetail />} />
-        <Route path="calendar" element={<CalendarView />} />
-        <Route path="summary" element={<Summary />} />
-      </Route>
-    </Routes>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,19 +1,21 @@
 import { Routes, Route } from "react-router";
-
-import MainLayout from "../layouts/MainLayout";
-
+import { MainLayout } from "../layouts/MainLayout";
 import Dashboard from "../pages/Dashboard";
-import Calendar from "../pages/Calendar";
 import Records from "../pages/Records";
-
+import AddTransaction from "../pages/AddTransaction";
+import TransactionDetail from "../pages/TransactionDetail";
+import CalendarView from "../pages/CalendarView";
+import Summary from "../pages/Summary";
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Dashboard />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="calendar" element={<Calendar />} />
         <Route path="records" element={<Records />} />
+        <Route path="add" element={<AddTransaction />} />
+        <Route path="transaction/:id" element={<TransactionDetail />} />
+        <Route path="calendar" element={<CalendarView />} />
+        <Route path="summary" element={<Summary />} />
       </Route>
     </Routes>
   );
